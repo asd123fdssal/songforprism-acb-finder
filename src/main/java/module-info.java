f3c5f4e2664b7f375387aa56cdd7588d5f6aa268
ARUSD <asd123fdssal@naver.com>
2025-01-30 20:26:35 +0900

    기타 수정

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
-module com.arsud.acbfinder {
+module ProcessApplication {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
 
     opens com.arsud.acbfinder to javafx.fxml;
     exports com.arsud.acbfinder;
